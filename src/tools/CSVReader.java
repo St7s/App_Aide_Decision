499a72c2553b6845c66c90b29119515f88a9c6e3
Brugel Adrien <adrien.brugel@alumni.univ-avignon.fr>
2017-10-25 16:47:25 +0200

    Try to push !!!

--- src/tools/CSVReader.java
+++ src/tools/CSVReader.java
@@ -7,51 +7,45 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVReader {
-	
-	
-	
-	
-	
+
 	public static int[][] giveMeTab(String filePath, String separator) {
-        String csvFile = "data/vote_1.csv";
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
+		String csvFile = "data/vote_1.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
 
-        try {
+		try {
 
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
 
-                // use comma as separator
-                String[] country = line.split(cvsSplitBy);
+				// use comma as separator
+				String[] country = line.split(cvsSplitBy);
 
-                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+				System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
 
-            }
+			}
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
-		
-		
 		return null;
 	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    	CSVReader.giveMeTab("data/vote_1.csv", ",");
-    }
+		CSVReader.giveMeTab("data/vote_1.csv", ",");
+	}
 
 }
