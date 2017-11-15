f6d8a395e15fb8a2dc64e0508e5c3cc99284f69d
Brugel Adrien <adrien.brugel@alumni.univ-avignon.fr>
2017-11-15 17:27:39 +0100

    Add vote_4.csv

--- src/test/debug/TestCondorcet.java
+++ src/test/debug/TestCondorcet.java
@@ -1,6 +1,5 @@
 package test.debug;
 
-import methode.Borda;
 import methode.Condorcet;
 import tools.Affichage;
 import tools.CSVReader;
@@ -9,19 +8,15 @@ public class TestCondorcet {
 
 	public static void main(String[] args) {
 
-		int[][] donnees = CSVReader.giveMeTab("data/vote_3.csv", ",");
-		// int[][] donnees = { { 3, 3, 3, 1 }, { 2, 2, 1, 3 }, { 1, 1, 2, 2 } };
+		int[][] donnees = CSVReader.giveMeTab("data/vote_4.csv", ",");
 
 		Affichage.afficheDonnees(donnees);
 		Condorcet condorcet = new Condorcet(donnees);
 
 		condorcet.calculCondorcet();
-		
-		System.out.println(condorcet.getListDuel());
-		
-		
 
-		
+		System.out.println(condorcet.getListDuel());
+
 	}
 
 }
