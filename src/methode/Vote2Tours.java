fbc506d5c00a3e370765c2ec04d5a1c468fbb956
Brugel Adrien <adrien.brugel@alumni.univ-avignon.fr>
2017-12-03 22:19:01 +0100

    Format !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/methode/Vote2Tours.java
+++ src/methode/Vote2Tours.java
@@ -13,25 +13,26 @@ public class Vote2Tours {
 		int[] resultVote2Tours = new int[2];
 
 		int[] classementVote1tour = Vote1Tour.giveMeRank(resultVote1Tour);
-		
-  	    // on teste si le meilleur candidat à la majourité absolu (+50) si oui il est ganagnt si non on passe au 2eme tour
-		if(classementVote1tour[0]>(nbVotant/2)) { 
-			
-			return classementVote1tour;
-		}
-	  else {
-	  boolean condition = true;
-  
 
-		// on test la 1ere valeur de la premiere colonne si la valeur egale à l'un de nos gangants du 1 tour,
-		// on ajoute 1 au candidat concerne, et on passe à la 2eme colonne
-		// si non on passe a la deuxieme ligne de la meme colonne et on retest a nouveau jusqu'a trouver un vote pour
-		// l'un des candidats
+		// on teste si le meilleur candidat à la majourité absolu (+50) si oui il est ganagnt si non on passe au 2eme
+		// tour
+		if (classementVote1tour[0] > (nbVotant / 2)) {
+
+			return classementVote1tour;
+		} else {
+			boolean condition = true;
+
+			// on test la 1ere valeur de la premiere colonne si la valeur egale à l'un de nos gangants du 1 tour,
+			// on ajoute 1 au candidat concerne, et on passe à la 2eme colonne
+			// si non on passe a la deuxieme ligne de la meme colonne et on retest a nouveau jusqu'a trouver un vote
+			// pour
+			// l'un des candidats
 
 			for (int j = 0; j < nbVotant; j++) {
 				int i = 0;
-	
-				while ((donnees[i][j] == classementVote1tour[0] || donnees[i][j] == classementVote1tour[1]) && condition) {
+
+				while ((donnees[i][j] == classementVote1tour[0] || donnees[i][j] == classementVote1tour[1])
+						&& condition) {
 					if (donnees[i][j] == classementVote1tour[0]) {
 						resultVote2Tours[0] += 1;
 						condition = false;
@@ -43,18 +44,18 @@ public class Vote2Tours {
 					i++;
 				}
 			}
-		
-	  }
-		// mise a jour du tableau de classement 
+
+		}
+		// mise a jour du tableau de classement
 		int temp;
-		
+
 		if (resultVote2Tours[0] > resultVote2Tours[1])
 			return classementVote1tour;
 		else
-			    temp = classementVote1tour[0];
-				classementVote1tour[0]=classementVote1tour[1];
-				classementVote1tour[1]=temp;
-		
+			temp = classementVote1tour[0];
+		classementVote1tour[0] = classementVote1tour[1];
+		classementVote1tour[1] = temp;
+
 		return classementVote1tour;
 
 	}
